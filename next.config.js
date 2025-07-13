4f6bc9e26f2cc2677a53f89fc2c7d754239513d2
divyadityabhardwaj <daditya.boss@gmail.com>
2025-07-13 21:13:45 +0530

    fix : config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const repo = 'divyadityabhardwaj.github.io'; // your repo name
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   images: {
     unoptimized: true,
   },
